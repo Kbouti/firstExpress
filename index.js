81a8f2788162fd91a5cdc1b4bcd88257d6221814
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-14 13:40:42 -0700

    Create jade.test file to render multiple pages

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-// First we import (require) the express module
-const app = express();
-// Then we use it to create a server (app)
-const port = 3000;
-// Which listens for requests on port 3000 and responds explaining what browser URL you can use to test the server
+// const express = require("express");
+// // First we import (require) the express module
+// const app = express();
+// // Then we use it to create a server (app)
+// const port = 3000;
+// // Which listens for requests on port 3000 and responds explaining what browser URL you can use to test the server
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port: ${port}!`);
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening on port: ${port}!`);
+// });
 
 // Next we can start the server by calling node index.js
 // When we do this we get our message logged to the console, and our page visible at: http://localhost:3000/
@@ -25,3 +25,10 @@ app.listen(port, () => {
 // Ok we did some stuff and I think xpress created a server for us in helloworld?
 // Here's the command we ran to activate the server:
 // DEBUG=helloworld:* npm start
+
+
+
+// Ok I did something...... I made a test.jade file and put some html in there, then established a corresponding rout in index.js
+// I believe this is essentially what I'm supposed to do for the assignment. 
+// It does't quite seem right tho because it doesn't seem easy or well organized? Idk....
+// I think i should have initiated hello world in the parent directory. I think I made a Express app in a node app?
