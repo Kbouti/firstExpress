fec927455cdb6ffd7302fbae291cbdf121844316
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-14 12:30:22 -0700

    Initiate helloworld server

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ app.listen(port, () => {
 
 // Next we can start the server by calling node index.js
 // When we do this we get our message logged to the console, and our page visible at: http://localhost:3000/
+
+
+// Next up we're askesd to install esLint
+// Then we're asked to make a script to make running esLint on our js files easier. I'm not running that code at the moment as the file structure does not seem to match what I have
+// Next up is: Installing the Express Application Generator
+// npm install express-generator -g
+
+// Ok we did some stuff and I think xpress created a server for us in helloworld?
+// Here's the command we ran to activate the server:
+// DEBUG=helloworld:* npm start
